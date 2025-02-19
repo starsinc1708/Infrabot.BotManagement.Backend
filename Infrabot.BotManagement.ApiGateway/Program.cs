0ea71e9372d81a96767010ec70b910d6349b3930
starsinc1708 <starsinc12@yandex.ru>
2025-02-19 22:27:47 +0400

    chatgpt 2

--- Infrabot.BotManagement.ApiGateway/Program.cs
+++ Infrabot.BotManagement.ApiGateway/Program.cs
@@ -1,24 +1,28 @@
+using System.Net;
 using System.Text.Json.Serialization;
 using Infrabot.BotManagement.ApiGateway;
 using Infrabot.BotManagement.ApiGateway.Endpoints;
 using Infrabot.BotManagement.Broker.Kafka;
 using Infrabot.BotManagement.Domain;
-using Microsoft.AspNetCore.HttpOverrides;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.Listen(IPAddress.Any, 5002); // HTTP
+    options.Listen(IPAddress.Any, 6002, listenOptions =>
+    {
+        listenOptions.UseHttps("/etc/ssl/certs/infrabot.ru.crt", "/etc/ssl/certs/infrabot.ru.key");
+    });
+});
+
 
 builder.Services.ConfigureHttpJsonOptions(options =>
 {
     options.SerializerOptions.PropertyNamingPolicy = null;
     options.SerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
     options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-});
-
-// Добавляем поддержку Forwarded Headers (для работы за прокси)
-builder.Services.Configure<ForwardedHeadersOptions>(options =>
-{
-    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
 });
 
 builder.Services.AddOpenApi();
@@ -28,21 +32,11 @@ builder.Services.AddSingleton<KafkaProducer>();
 
 var app = builder.Build();
 
-app.UseForwardedHeaders();
+app.UseHttpsRedirection();
 
-
-// Настраиваем маршруты
 app.MapTelegramApiEndpoints();
 app.MapTelegramUpdatesEndpoints();
 app.MapOpenApi();
 app.MapScalarApiReference();
 
-// Исправленный редирект, чтобы он учитывал прокси
-app.MapGet("/", (HttpContext ctx) =>
-{
-    var prefix = ctx.Request.Headers["X-Forwarded-Prefix"].FirstOrDefault() ?? "";
-    return Results.Redirect($"{prefix}/scalar");
-});
-
-// Запуск с явным указанием порта
-app.Run("https://0.0.0.0:5002");
+app.Run();
